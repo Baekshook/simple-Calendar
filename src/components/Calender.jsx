79cdf652c990010c9a92d8b49e035310464b8bcc
Baekshook <ecgo0106@naver.com>
2023-04-03 11:40:13 +0900

    Date.jsx CSS_1

--- src/components/Calender.jsx
+++ src/components/Calender.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Calender() {
   return (
-    <div className='bg-white w-1/2 rounded-lg shadow-2xl'>Calender</div>
+    <div className='bg-white w-1/2 rounded-lg shadow-2xl p-8'>Calender</div>
   )
 }
---
+++ src/components/Date.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Date() {
+  return (
+    <li className="inline-block bg-white rounded-lg shadow-md">
+      <div className=" bg-red-400 rounded-t-xl px-2 py-1 text-xl text-white">
+        2
+      </div>
+      <ul className="p-2 text-lg">
+        <li>🧹 청소하기</li>
+        <li>👕 빨래하기</li>
+      </ul>
+    </li>
+  );
+}
