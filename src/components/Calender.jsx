3d465745add1e44de31201fdf57fb5b9c591c9d8
Baekshook <ecgo0106@naver.com>
2023-04-03 11:42:25 +0900

    Calender CSS_2

--- src/components/Calender.jsx
+++ src/components/Calender.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import Date from './Date'
 
 export default function Calender() {
   return (
-    <div className='bg-white w-1/2 rounded-lg shadow-2xl p-8'>Calender</div>
+    <div className='bg-white w-1/2 rounded-lg shadow-2xl p-8'>
+        <div className='pb-8 text-4xl font-bold'>2023, 04</div>
+        <Date />
+    </div>
   )
 }
