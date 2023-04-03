99773adb7b1a588455901bf708083b7e80323181
Baekshook <ecgo0106@naver.com>
2023-04-03 14:35:25 +0900

    삼항연산자 적용, CSS 추가 작업

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import Date from './Date'
-import calendarData from "../calendarData.json"
+import React from "react";
+import Date from "./Date";
+import calendarData from "../calendarData.json";
 
 export default function Calender() {
   return (
-    <div className='bg-white w-1/2 rounded-lg shadow-2xl p-8'>
-        <div className='pb-8 text-4xl font-bold'>2023, 04</div>
-       {
-        calendarData.map((v,i) => {
-          return <Date key={i} date={v.date} day={v.day} todos={v.todos}/>
-        })
-       }
+    <div className="bg-white w-1/2 rounded-lg shadow-2xl p-8">
+      <div className="pb-8 text-4xl font-bold">2023, 04</div>
+      <ul className="grid grid-cols-7 gap-4">
+        {calendarData.map((v, i) => {
+          return <Date key={i} date={v.date} day={v.day} todos={v.todos} />;
+        })}
+      </ul>
     </div>
-  )
+  );
 }
--- src/components/Date.jsx
+++ src/components/Date.jsx
@@ -3,10 +3,14 @@ import React from "react";
 export default function Date(props) {
   return (
     <li className="inline-block bg-white rounded-lg shadow-md">
-      <div className={`bg-red-400 rounded-t-xl px-2 py-1 text-xl text-white`}>
+      <div
+        className={`${
+          props.day === "SUN" ? "bg-red-400" : props.day === "SAT" ? "bg-orange-400" : "bg-blue-400"
+        } rounded-t-xl px-2 py-1 text-xl text-white`}
+      >
         {props.date}
       </div>
-      <ul className="p-2 text-lg">
+      <ul className="p-2 text-lg h-20">
         {props.todos.map((v, i) => {
           return <li>{v}</li>;
         })}
