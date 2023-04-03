e6d1039e1bd836a2addcb748fb89a5165a9a1d34
Baekshook <ecgo0106@naver.com>
2023-04-03 13:37:28 +0900

    map함수로 json값 호출

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import Date from './Date'
+import calendarData from "../calendarData.json"
 
 export default function Calender() {
   return (
     <div className='bg-white w-1/2 rounded-lg shadow-2xl p-8'>
         <div className='pb-8 text-4xl font-bold'>2023, 04</div>
-        <Date />
+       {
+        calendarData.map((v,i) => {
+          return <Date />
+        })
+       }
     </div>
   )
 }
